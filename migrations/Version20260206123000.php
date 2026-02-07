ef3da3503b9827e1907f2e4391a1926f9422fc5f
montaassarr <montamsallem@gmail.com>
2026-02-07 15:22:58 +0100

    Fix migration role_id change

--- migrations/Version20260206123000.php
+++ migrations/Version20260206123000.php
@@ -16,11 +16,11 @@ final class Version20260206123000 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE users MODIFY first_name VARCHAR(100) NULL, MODIFY last_name VARCHAR(100) NULL, MODIFY phone VARCHAR(20) NULL, MODIFY region_id INT NULL, MODIFY role_id INT NULL');
+        $this->addSql('ALTER TABLE users MODIFY first_name VARCHAR(100) NULL, MODIFY last_name VARCHAR(100) NULL, MODIFY phone VARCHAR(20) NULL, MODIFY region_id INT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE users MODIFY first_name VARCHAR(100) NOT NULL, MODIFY last_name VARCHAR(100) NOT NULL, MODIFY phone VARCHAR(20) NOT NULL, MODIFY region_id INT NOT NULL, MODIFY role_id INT NOT NULL');
+        $this->addSql('ALTER TABLE users MODIFY first_name VARCHAR(100) NOT NULL, MODIFY last_name VARCHAR(100) NOT NULL, MODIFY phone VARCHAR(20) NOT NULL, MODIFY region_id INT NOT NULL');
     }
 }
